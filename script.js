5c25aaeb92780bab450c6850626f9a1a95f1f4f6
Vinh <vinh.bui0101@gmail.com>
2022-01-13 23:57:27 -0800

    Finished best out of five round component (loop)

--- script.js
+++ script.js
@@ -1,28 +1,50 @@
-const option = ["rock", "paper", "scissors"]; 
+const option = ["rock", "paper", "scissor"]; 
+var playerScore = 0;
+var computerScore = 0;
 function computerPlay(){ 
   return option[Math.floor(Math.random()*option.length)] 
 }; 
 function playerSelect() {
   const prompt = require('prompt-sync')();
-  const answer = prompt('Rock paper or scissor?');
+  let answer = prompt('Rock paper or scissor?');
   return answer;
-}
-const playerSelection = playerSelect();
-const computerSelection = computerPlay();
+};
 function playRound(playerSelection, computerSelection) { 
   if(playerSelection == computerSelection){ 
-    return ("tied game"); 
+    return("tied game");
   } else if
-  ((computerSelection == "rock" && playerSelection == "scissors") || 
-    (computerSelection == "scissors" && playerSelection == "paper") || 
+  ((computerSelection == "rock" && playerSelection == "scissor") || 
+    (computerSelection == "scissor" && playerSelection == "paper") || 
     (computerSelection == "paper" && playerSelection == "rock")){ 
-    return ("You lost"); 
+    return("You lost"); 
   }else if 
-  ((playerSelection == "rock" && computerSelection == "scissors") || 
-  (playerSelection == "scissors" && computerSelection == "paper") || 
+  ((playerSelection == "rock" && computerSelection == "scissor") || 
+  (playerSelection == "scissor" && computerSelection == "paper") || 
   (playerSelection == "paper" && computerSelection == "rock") ){
-    return ("You won");
-  } 
+    return("You won");
+  }else{
+    console.log("try again");
+  }
 }; 
-
-console.log(playRound(playerSelection, computerSelection))
+function game(){
+  while(true){
+    const playerSelection = playerSelect();
+    const computerSelection = computerPlay();
+    let rounds = playRound(playerSelection, computerSelection);
+    if(rounds == "You won"){
+      playerScore++;
+      console.log(`You won, score is ${playerScore} to ${computerScore}`);
+    }else if (rounds == "You lost"){
+      computerScore++;
+      console.log(`You lost, score is ${playerScore} to ${computerScore}`);
+    }else{
+      console.log(`tied game, score is ${playerScore} to ${computerScore}`); 
+    }
+    if(playerScore == 5){
+      return ("You beat the computer");
+    }else if(computerScore == 5){
+      return ("You lost to the computer");   
+    }
+  }
+}
+console.log(game())
