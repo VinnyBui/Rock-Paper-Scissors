3e6d1d522f36f0673bad8ab270157f550ad79ca8
Vinh <vinh.bui0101@gmail.com>
2022-01-13 20:08:30 -0800

    basic logic

---
+++ script.js
@@ -0,0 +1,23 @@
+const option = ["rock", "paper", "scissors"]; 
+function computerPlay(){ 
+  return option[Math.floor(Math.random()*option.length)] 
+};
+const playerSelection = 'rock';
+const computerSelection = computerPlay(); 
+function playRound(playerSelection, computerSelection) { 
+  if(playerSelection == computerSelection){ 
+    return ("tied game"); 
+  } else if
+  ((computerSelection == "rock" && playerSelection == "scissors") || 
+    (computerSelection == "scissors" && playerSelection == "paper") || 
+    (computerSelection == "paper" && playerSelection == "rock")){ 
+    return ("You lost"); 
+  }else if 
+  ((playerSelection == "rock" && computerSelection == "scissors") || 
+  (playerSelection == "scissors" && computerSelection == "paper") || 
+  (playerSelection == "paper" && computerSelection == "rock") ){
+    return ("You won");
+  } 
+}; 
+
+console.log(playRound(playerSelection, computerSelection))
